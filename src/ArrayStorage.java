d1386f93df1fa3714fab105d798f70a85fcf4d46
krutaxe <krutaxe@mail.ru>
2024-10-03 12:41:30 +0300

    implementation_hw1-fix1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,32 +5,32 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
         for (int i = 0; i <= size(); i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
         storage[size()] = r;
+        size++;
     }
 
     Resume get(String uuid) {
         int index = findIndex(uuid);
-        if (index < 0) {
-            return new Resume();
-        }
-        return storage[index];
+        return index < 0 ? null : storage[index];
     }
 
     void delete(String uuid) {
-        int size = size();
         int index = findIndex(uuid);
         if (storage[index].uuid.equals(uuid)) {
             System.arraycopy(storage, index + 1, storage, index, size - index - 1);
             storage[size - 1] = null;
         }
+        size--;
     }
 
     /**
@@ -45,13 +45,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int size = 0;
-        for (Resume resume : storage) {
-            if (resume == null) {
-                break;
-            }
-            size++;
-        }
         return size;
     }
 
